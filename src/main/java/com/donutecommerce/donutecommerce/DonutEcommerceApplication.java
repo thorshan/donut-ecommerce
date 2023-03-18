1e333faf8f071c74e17286cfa611cc01e42eae97
thorshan <myothunaing18@gmail.com>
2023-03-18 18:33:33 +0630

    First Commit

---
+++ src/main/java/com/donutecommerce/donutecommerce/DonutEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.donutecommerce.donutecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonutEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donutecommerce/donutecommerce/DonutEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donutecommerce.donutecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
