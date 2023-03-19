67753561d0508f787bdfc82b9726a65ffc5180c0
DESKTOP-VEUQUHV\User <wp67636@gmail.com>
2023-03-19 19:49:36 +0630

    Wai Phyo Login And Register

--- src/main/java/com/donutecommerce/donutecommerce/entity/Category.java
+++ src/main/java/com/donutecommerce/donutecommerce/entity/Category.java
@@ -1,10 +1,14 @@
 package com.donutecommerce.donutecommerce.entity;
 
 
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "category")
 public class Category {
+    @Id
+    private int id;
 }
