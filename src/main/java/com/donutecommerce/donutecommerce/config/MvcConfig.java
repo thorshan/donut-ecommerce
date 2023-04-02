03c295239eb41170caf536ea0047d45eb5993767
thorshan <myothunaing18@gmail.com>
2023-04-02 12:24:58 +0630

    Revamp link errors and combine css into one link

--- src/main/java/com/donutecommerce/donutecommerce/config/MvcConfig.java
+++ src/main/java/com/donutecommerce/donutecommerce/config/MvcConfig.java
@@ -1,13 +1,11 @@
 package com.donutecommerce.donutecommerce.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
+@Controller
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
